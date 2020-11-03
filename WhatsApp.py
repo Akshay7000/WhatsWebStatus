e92f9c2875352d9917499454c205532478d9c05b
Akshay Patel <44333401+Akshay7000@users.noreply.github.com>
2020-11-03 13:25:06 +0530

    Update WhatsApp.py

--- WhatsApp.py
+++ WhatsApp.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from playsound import playsound
-driver = webdriver.Chrome('F:\Hacks\WhatsAppOnline\chromedriver.exe')
+driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com')
 time.sleep(10)
 a=0
@@ -14,7 +14,7 @@ def online():
 		element_status = driver.find_element_by_xpath("//*[@id='main']/header/div[2]/div[2]/span").get_attribute("innerHTML").splitlines()[0]
 	
 		if  (element_status == "online"):
-			playsound('F:\Hacks\WhatsAppOnline\play.mp3')
+			playsound('play.mp3')
 		
 		'''text_box = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]")
 		text_box.send_keys("You are under surveillance. Current Status "+ element_status)
