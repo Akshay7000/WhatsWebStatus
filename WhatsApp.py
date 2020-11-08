a022fa2349155df87838c526f96d974337106f10
Akshay Patel <44333401+Akshay7000@users.noreply.github.com>
2020-11-08 23:31:56 +0530

    Update WhatsApp.py

--- WhatsApp.py
+++ WhatsApp.py
@@ -1,33 +1,39 @@
 from selenium import webdriver
 import time
 from playsound import playsound
+
+name = input("Enter the contact name: ")
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com')
 time.sleep(10)
-a=0
-#target_contact = driver.find_element_by_xpath("//span[text()='AKSHAY']").click()
-#replace AKSHAY with contact name
 
+
+target_contact = driver.find_element_by_xpath("//span[.='" + name + "']").click()
+
+play = "play"
+offline = "off"
 def online():
-	global a
-	def status():	
-		
+	global a,play,offline
+
+	def status():
+		global play,offline
 		element_status = driver.find_element_by_xpath("//*[@id='main']/header/div[2]/div[2]/span").get_attribute("innerHTML").splitlines()[0]
-	
-		if  (element_status == "online"):
-			playsound('play.mp3')
-		
-		'''text_box = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]")
-		text_box.send_keys("You are under surveillance. Current Status "+ element_status)
-		send_button = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
-		'''
-		
+		if (element_status == "online"):
+			contact_name = driver.find_element_by_xpath("//*[@id='main']/header/div[2]/div/div/span").text
+			if (play == "play"):
+				print(contact_name +" is online")
+				playsound('play.mp3')
+				play = "noplay"
+				offline = "off"
 	try:
 		status()
 	except:
-		a = 0
-		
-	
-while a==0:
+		if (offline == "off"):
+				print("offline")
+				#playsound('play.mp3')
+				offline = "on"
+		else:
+			play = "play"
+
+while true:
 	online()
-	
